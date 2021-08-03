6f9c764826724303ba373d91a742be07668943f0
OlaeruMihai <guban_89@yahoo.com>
2021-08-03 20:23:36 +0300

    Second commit

--- tema-curs3/main.py
+++ tema-curs3/main.py
@@ -1,19 +1,18 @@
 list_numbers = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 print(list_numbers)
 
-list_numbers_ascending = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
-list_numbers_ascending.sort()
-print(list_numbers_ascending)
+list_numbers.sort()
+print('List numbers ascending', list_numbers)
 
-list_numbers_descending = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
-list_numbers_descending.sort(reverse = True)
-print(list_numbers_descending)
+list_numbers.sort(reverse = True)
+print('List numbers descending', list_numbers)
 
-my_sliced_list_even = list_numbers_ascending[1::2]
+list_numbers=list_numbers[::-1]
+my_sliced_list_even = list_numbers[1::2]
 print(my_sliced_list_even)
 
-my_sliced_list_odd = list_numbers_ascending[::2]
+my_sliced_list_odd = list_numbers[::2]
 print(my_sliced_list_odd)
 
-my_sliced_list_multiples = list_numbers_ascending [2::3]
+my_sliced_list_multiples = list_numbers [2::3]
 print(my_sliced_list_multiples)
